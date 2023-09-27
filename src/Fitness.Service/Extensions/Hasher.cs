999ef5346b7e0f446a1532065397d2e712580a5a
Iskandar Kodirov <eiscocyber@gmail.com>
2023-09-27 23:49:01 +0500

    Exceptions added and Extensions writing

---
+++ src/Fitness.Service/Extensions/Hasher.cs
@@ -0,0 +1,15 @@
+namespace Fitness.Service.Extensions;
+
+public static class Hasher
+{
+    public static string Hash(this string password)
+    {
+        string hashedPassword = BCrypt.Net.BCrypt.HashPassword(password);
+        return hashedPassword;
+    }
+
+    public static bool Verify(this string password, string hashedPassword)
+    {
+        return BCrypt.Net.BCrypt.Verify(password, hashedPassword);
+    }
+}
